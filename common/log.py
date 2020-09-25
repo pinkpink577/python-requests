d1656361d0431757b2a1d4fc88d90a356ee4dcbd
pinkpink577 <499200305@qq.com>
2020-09-25 17:26:47 +0800

    fix：写错了类别

--- common/log.py
+++ common/log.py
@@ -4,30 +4,30 @@ import logging
 from datetime import datetime
 import threading
 
-class log:
-    class Log:
-        def __init__(self):
-            global logPath, resultPath, proDir
-            proDir = readConfig.proDir
-            resultPath = os.path.join(proDir, "result")
-            # create result file if it doesn't exist
-            if not os.path.exists(resultPath):
-                os.mkdir(resultPath)
-            # defined test result file name by localtime
-            logPath = os.path.join(resultPath, str(datetime.now().strftime("%Y%m%d%H%M%S")))
-            # create test result file if it doesn't exist
-            if not os.path.exists(logPath):
-                os.mkdir(logPath)
-            # defined logger
-            self.logger = logging.getLogger()
-            # defined log level
-            self.logger.setLevel(logging.INFO)
 
-            # defined handler
-            handler = logging.FileHandler(os.path.join(logPath, "output.log"))
-            # defined formatter
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            # defined formatter
-            handler.setFormatter(formatter)
-            # add handler
-            self.logger.addHandler(handler)
+class Log:
+    def __init__(self):
+        global logPath, resultPath, proDir
+        proDir = readConfig.proDir
+        resultPath = os.path.join(proDir, "result")
+        # create result file if it doesn't exist
+        if not os.path.exists(resultPath):
+            os.mkdir(resultPath)
+        # defined test result file name by localtime
+        logPath = os.path.join(resultPath, str(datetime.now().strftime("%Y%m%d%H%M%S")))
+        # create test result file if it doesn't exist
+        if not os.path.exists(logPath):
+            os.mkdir(logPath)
+        # defined logger
+        self.logger = logging.getLogger()
+        # defined log level
+        self.logger.setLevel(logging.INFO)
+
+        # defined handler
+        handler = logging.FileHandler(os.path.join(logPath, "output.log"))
+        # defined formatter
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # defined formatter
+        handler.setFormatter(formatter)
+        # add handler
+        self.logger.addHandler(handler)
